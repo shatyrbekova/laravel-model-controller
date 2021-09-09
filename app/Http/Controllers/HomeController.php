550c3bd4cdc026d66a7d43733359e5efedeb911f
shatyrbekova <perizat.shatyrbekova@gmail.com>
2021-09-09 16:28:30 +0200

    fsecond commit

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,11 +4,16 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use APP\Movie;
 class HomeController extends Controller
 {
     // 
     public function index(){
-        return view('welcome');
+        // return view('welcome');
+       
+        $allMovies = Movie::all();
+        dump($allMovies);
+
     }
      
 }
---
+++ app/Movie.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Movie extends Model
+{
+    //
+    // public $id;
+    // public $title;
+    // public $original_title;
+    // public $nationality;
+    // public $date;
+    // public $cover;
+    // public $price;
+    // public $abstract;
+}
